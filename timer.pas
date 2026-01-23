23dc3a5e4ccf1a643c3d6e3b9cd9dd81ee286b53
@haru <haru@nya.com>
2026-01-23 16:19:49 +0500

    add base time counter

--- timer.pas
+++ timer.pas
@@ -9,6 +9,11 @@ uses
 
 type
 
+  TTimePeriod = record
+    Name, Sound: String;
+    Time: Integer;
+  end;
+
   { TFrameTimer }
 
   TFrameTimer = class(TFrame)
@@ -18,15 +23,78 @@ type
     LabelTitle: TLabel;
     PanelCounter: TPanel;
     PanelControl: TPanel;
-  private
-
+    TimerCount: TTimer;
+    procedure ButtonPauseClick(Sender: TObject);
+    procedure ButtonStopClick(Sender: TObject);
+    procedure TimerCountTimer(Sender: TObject);
+  protected
+    TimeMs: Cardinal;
+    procedure ResetTimer;
+    procedure ShowTime;
   public
-
+    procedure AddPeriod(AData: TTimePeriod);
+    procedure Start;
   end;
 
 implementation
 
 {$R *.lfm}
+
+{ TFrameTimer }
+
+procedure TFrameTimer.AddPeriod(AData: TTimePeriod);
+begin
+
+end;
+
+procedure TFrameTimer.Start;
+begin
+  ResetTimer;
+  TimerCount.Enabled:= True;
+end;
+
+procedure TFrameTimer.ResetTimer;
+begin
+  TimeMs:= 0;
+  ShowTime;
+end;
+
+procedure TFrameTimer.TimerCountTimer(Sender: TObject);
+begin
+  TimeMs:= TimeMs + TimerCount.Interval;
+  ShowTime;
+end;
+
+procedure TFrameTimer.ButtonPauseClick(Sender: TObject);
+begin
+  if TimerCount.Enabled then
+  begin
+    TimerCount.Enabled:= False;
+    ButtonPause.Caption:= 'Continue';
+  end
+  else
+  begin
+    TimerCount.Enabled:= True;
+    ButtonPause.Caption:= 'Pause';
+  end;
+
+end;
+
+procedure TFrameTimer.ButtonStopClick(Sender: TObject);
+begin
+  TimerCount.Enabled:= False;
+  ResetTimer;
+end;
+
+procedure TFrameTimer.ShowTime;
+var
+  min, sec: Cardinal;
+begin
+  sec:= TimeMs div 1000;
+  min:= sec div 60;
+  sec:= sec - (min * 60);
+  LabelTime.Caption:= IntToStr(min) + ':' + IntToStr(sec);
+end;
 
 end.
 
