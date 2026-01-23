06413ee04d1ac6d694734fd4984bb11605d9a1b7
@haru <haru@nya.com>
2026-01-23 14:37:36 +0500

    start

---
+++ main.pas
@@ -0,0 +1,26 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFormTTimer = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FormTTimer: TFormTTimer;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
